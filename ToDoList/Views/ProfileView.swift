1c076bf6a727063d3a399145a5ab6675f3950319
timmy-ctrl <timmygithub@gmail.com>
2025-01-31 05:48:49 +0800

    first commit

---
+++ ToDoList/Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  ToDoList
+//
+//  Created by Тимофей Олегович on 31.01.2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
---
+++ ToDoList/Views/ToDoListView.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoListItemsView.swift
+//  ToDoList
+//
+//  Created by Тимофей Олегович on 31.01.2025.
+//
+
+import SwiftUI
+
+struct ToDoListItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoListItemsView()
+}
